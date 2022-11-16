863960087b2dc35f6400f97a264defdef62cadee
kenanoestreich <kenano1999@gmail.com>
2022-11-15 19:02:58 -0600

    feat: add file-system nav and other niceties

--- SLAKES/CalculateAggregateStability.R
+++ SLAKES/CalculateAggregateStability.R
@@ -1,13 +1,13 @@
-#This sample code is Appendix A for the Soil Health Institute's Standard Operating Procedure for Wet Aggregate Stability by Image Quantification. 
-#The code was last updated on May 16th, 2022. Email eriekie@soilhealthinstitue.org with questions. 
+# This script was built off the sample code in Appendix A for the Soil Health Institute's Standard Operating Procedure for Wet Aggregate Stability by Image Quantification. 
+# Email koestreich@danforthcenter.org with questions. 
 
-#The code uses two images of a petri dish containing three soil aggregates. One image is taken at the start and one is taken at the end of a 10 minute 
-#submersion in water.The code calculates the mean aggregate stability for the three aggregates in the petri dish by 1)cropping the images, 2)converting them to 
-#grayscale, 3) converting to binary by using a threshold obtained using Otsu's method (this value represents the cutoff value between water and aggregate pixels) 
-#and then 4) dividing the total initial area of aggregates (not individual aggregate areas) by the total final are of aggregates. In step 5), an example of how 
-#to save data are is given. 
+# The code uses two images of a petri dish containing three soil aggregates. One image is taken at the start and one is taken at the end of a 10 minute 
+# submersion in water (prior to this script). The code calculates the mean aggregate stability for the three aggregates in the petri dish by 1)cropping the images 
+# (using hard-coded boundaries - POSSIBLE SOURCE OF ERROR), 2)converting them to grayscale, 3) converting to binary by using a threshold obtained using Otsu's method
+# (this value represents the cutoff value between water and aggregate pixels) and then 4) dividing the total initial area of aggregates (not individual aggregate 
+# areas) by the total final area of aggregates. 
 
-#You will need to update file paths to locate the images on your computer lines and to save output.
+
 
 # If user doesn't have BiocManager installed, install it
 if (!require("BiocManager", quietly = TRUE)) { 
@@ -27,23 +27,62 @@ if (!require("stringr", quietly = TRUE)) {
   install.packages("stringr")
 }
 
+# Load in required packages
 library(stringr)
 library(EBImage)
 library(dplyr)
 
-#path to folder with a subfolder for each pair of images 
-images_path <- readline(prompt = "Enter Absolute File Path for Folder Containing Image-Pair Subfolders: (Example: C:/Users/exampleUser/Data/AgStabData): ")
+# Path to folder with a subfolder for each pair of images 
+# POSSIBLE SOURCE OF ERROR: This script assumes a very specific folder structure
+# Namely, one parent folder containing 1 folder FOR EACH pair of images (initial and final)
+# In this step, you must select the PARENT FOLDER
+images_path <- choose.dir()
+
+# Prepare .csv file to store results  
+# This will name results with the year, month, day, hour, and minute
+processing_datetime <- format(Sys.time(), "%Y-%m-%d-%H%M")
+results_filename <- paste0(images_path, "/results_", processing_datetime, ".csv")
+
+#create .csv file if it does not exist or abort if it does exist to avoid overwriting
+if (!file.exists(results_filename)){
+  file.create(results_filename)
+} else {
+  cont <- ''
+  while (!(str_trim(cont) %in% c('yes', 'no'))){
+    cont <- readline(prompt = "Results .csv already exists. Continuing will overwrite existing file. Do you still want to continue? (type 'yes' or 'no') ")
+  }
+  if (cont == 'no'){
+    opt <- options(show.error.messages = FALSE)
+    on.exit(options(opt))
+    print('Aborting process')
+    stop()
+  } else if (cont == 'yes'){
+    file.remove(results_filename)
+    file.create(results_filename)
+  }
+}
+
+# Initialize dataframe that will eventually comprise the output .csv
+df <- data.frame(matrix(ncol = 2, nrow = 0))
+x <- c("Stability Index", "Initial Image Filename")
+colnames(df) <- x
+
+# Suppress warnings, because it doesn't like that we're "overwriting" column names
+suppressWarnings(write.table(df, file=results_filename, append = TRUE, row.names = FALSE))
 
 #get list of folders in image folder to iterate through
 folder_list <- list.dirs(path = images_path, recursive = FALSE)
 
 for (folder in folder_list) {
   
-  results_path <- folder
-  
+  # Get file paths of images
   image_list <- list.files(path = folder, full.names = TRUE, recursive = FALSE)
   img_FinalPath <- NA
   img_InitialPath <- NA
+  
+  if (length(image_list)!=2){
+    print(paste0("Warning: More than two files in subfolder ",folder,'. Continuing...'))
+  }
   
   #identify both images
   for (image in image_list) {
@@ -66,8 +105,8 @@ for (folder in folder_list) {
   # plot(img_Initial)
   img_Final <-  readImage(img_FinalPath)
   # plot(img_Final)
-
-
+  
+  
   #get the image dimensions to inform where to crop
   dim(img_Initial)
   dim(img_Final)
@@ -75,35 +114,27 @@ for (folder in folder_list) {
   #1 crop images so they only contain the three aggregates you intend to analyze.
   #It is good practice to leave the sample ID and replicate number in the image
   #If you already cropped your images before reading in, you need to rename them. 
-  #To rename your image, you need to comment out lines 39 and 41 and uncomment lines 34 and 35 and run them:
-  #img_Initial_crop <- img_Initial
-  #img_Final_crop <- img_Final 
+
   #No other aggregates should be in the image and no petri dish edges should be present.
   
   #you may have to guess and check to get the right crop. 
-  #img_Initial_crop <- img_Initial[1336:2736, 1124:2524]
-  #plot(img_Initial_crop)
   #plot to see if you got a good trim, if not, change numbers and try again. Note that images have three bands and the first is plotted by default. We will aveage over bands at the end.
-  #img_Final_crop <- img_Final[1336:2736, 1124:2524] 
-  #plot(img_Final_crop)
-  
-  incorrectDims <- tryCatch(
+
+  imgCropError <- tryCatch(
     expr = {
       img_Initial_crop <- img_Initial[1336:2736, 1124:2524]
-      # plot(img_Initial_crop)
+      plot(img_Initial_crop)
+      title(main = img_InitialPath)
       img_Final_crop <- img_Final[1336:2736, 1124:2524] 
-      # plot(img_Final_crop)
+      plot(img_Final_crop)
+      title(main = img_FinalPath)
     },
-    error = function(e){
-      message(paste0('Error; Skipping this folder; Ensure Images are 8-bit JPG and Check Cropping Boundaries for Folder: ', folder))
-      print(e)
-    }
+    error = function(e) e
   )
   
-  if(inherits(incorrectDims, "error")){
+  if(inherits(imgCropError, "error")){
     next
   }
-  
   
   #2 make images gray scale
   colorMode(img_Initial_crop) <- Grayscale
@@ -115,9 +146,9 @@ for (folder in folder_list) {
   #make binary versions of the images by selecting values less than the threshold (found in lines 48 and 49) and then summing the pixels that are 
   #greater than the threshold (lines 48 and 49). These are done for of the three each image bands. 
   img_binary_Initial <- EBImage::combine( mapply(function(frame, th) frame <= th, getFrames(img_Initial_crop), threshold_Initial, SIMPLIFY=FALSE) )
-  # plot(img_binary_Initial)
+  plot(img_binary_Initial)
   img_binary_Final <- EBImage::combine( mapply(function(frame, th) frame <= th, getFrames(img_Final_crop), threshold_Final, SIMPLIFY=FALSE) )
-  # plot(img_binary_Final)
+  plot(img_binary_Final)
   
   #area_Initial <- apply(img_binary_Initial, MARGIN = 3, sum, na.rm = T)
   
@@ -130,9 +161,8 @@ for (folder in folder_list) {
   #4 The stability index for this petri dish is initial area dived by the final area. The mean is taken across the three image bands.     
   StabilityIndex<-mean(area_Initial/area_Final)
   
-  #5 Example of how to save results    
-  #This will name results with the year, month, day, hour, and minute
-  processing_datetime <- format(Sys.time(), "%Y-%m-%d-%H%M")
-  #this will save the value to a CSV
-  write.csv(StabilityIndex, paste0(results_path, '/slaking_image_results_', processing_datetime, '.csv'), row.names = F)
+  df <- data.frame("Stability Index"=StabilityIndex, InitialImageFileName=img_InitialPath)
+  write.table(df, file=results_filename, append=TRUE, col.names = FALSE, row.names = FALSE)
 }
+
+print("Done Processing. See results in the generated csv file in the parent folder")
